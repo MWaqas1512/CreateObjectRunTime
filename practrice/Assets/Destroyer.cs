38c3ff04a65ef03213447bb44c1c737341105643
MWaqas1512 <64679036+MWaqas1512@users.noreply.github.com>
2020-06-10 12:24:36 +0500

    Mix Data of creating object on run time and pick and drop object in unity 3d.

---
+++ practrice/Assets/Destroyer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public float LifeTime = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LifeTime > 0)
+        {
+            LifeTime -= Time.deltaTime;
+            if (LifeTime <= 0)
+            {
+                destruction();
+            }
+        }
+
+        if (this.transform.position.y < -20)
+        {
+            destruction();
+        }
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.name == "destroyer")
+        {
+            destruction();
+        }
+    }
+
+    void destruction()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ practrice/Assets/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Spawner : MonoBehaviour
+{
+    //public Transform spawnPos;
+    //public GameObject spawnee;
+    public GameObject theObject;
+    public float maxPos= 200f;
+    public float minPos = 200f;
+
+    public void spawn() {
+        
+            
+            var theNewPos= new Vector3 (Random.Range(minPos,maxPos),0,Random.Range(minPos,maxPos));
+            GameObject go = Instantiate(theObject);
+            go.transform.position = theNewPos;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+            if (Input.GetButtonDown("createBall"))
+            {
+                spawn();
+                //Instantiate(spawnee,spawnPos.position,spawnPos.rotation);
+            }
+        
+    }
+}
